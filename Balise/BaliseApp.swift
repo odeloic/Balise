3fecd3c9dcdfbf7c406d4900cbeaa8eb9bd51b9a
Loic Ishimwe <manzii.ode@gmail.com>
2026-07-23 21:24:56 +0200

    Initial Commit

---
+++ Balise/BaliseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaliseApp.swift
+//  Balise
+//
+//  Created by Loic Ishimwe on 23.07.26.
+//
+
+import SwiftUI
+
+@main
+struct BaliseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
